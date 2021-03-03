ff54fdf114dfdf4ad877ca67cd2a8f9bfb5c5cac
Roynald Oktaviano <roynald.okta@gmail.com>
2021-03-03 15:08:57 +0700

    Header Progress
    Header Finshing without hover state

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,23 @@
 const hamburger = document.querySelector('.header-hamburger');
+const fadeElem = document.querySelectorAll('.fade-element');
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
 hamburger.addEventListener("click",function(){
-    if(hamburger.classList.contains('open')){
-        hamburger.classList.remove('open');
-    } else {
-        hamburger.classList.add('open');
+    if(header.classList.contains('open')){ //Close Hamburger
+        header.classList.remove('open');
+        body.classList.remove('no-scroll')
+        fadeElem.forEach(function(elem){
+            elem.classList.remove('fade-in')
+            elem.classList.add('fade-out');
+        })
+    } else { //Open Hamburger
+        header.classList.add('open');
+        body.classList.add('no-scroll')
+        fadeElem.forEach(function(elem){
+            elem.classList.remove('fade-out');
+            elem.classList.add('fade-in')
+            
+        })
     }
 })
