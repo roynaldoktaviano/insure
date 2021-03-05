ee1e2bae6c15dde880223239276801e3ebf6c787
Roynald Oktaviano <roynald.okta@gmail.com>
2021-03-05 11:51:10 +0700

    Hero Progress
    Hero Mobile Display Finished

--- app/js/script.js
+++ app/js/script.js
@@ -2,18 +2,22 @@ const hamburger = document.querySelector('.header-hamburger');
 const fadeElem = document.querySelectorAll('.fade-element');
 const header = document.querySelector('.header');
 const html = document.querySelector('html');
+const body = document.querySelector('body');
+
 
 hamburger.addEventListener("click",function(){
     if(header.classList.contains('open')){ //Close Hamburger
         header.classList.remove('open');
-        html.classList.remove('no-scroll')
+        html.classList.remove('no-scroll');
+        body.classList.remove('no-scroll');
         fadeElem.forEach(function(elem){
             elem.classList.remove('fade-in')
             elem.classList.add('fade-out');
         })
     } else { //Open Hamburger
         header.classList.add('open');
-        html.classList.add('no-scroll')
+        html.classList.add('no-scroll');
+        body.classList.add('no-scroll');
         fadeElem.forEach(function(elem){
             elem.classList.remove('fade-out');
             elem.classList.add('fade-in')
